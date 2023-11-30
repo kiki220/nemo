212466c8343ab7dfebf06b04d05a1f4b20bdf0ba
kiki220 <ygy911@naver.com>
2023-11-30 23:21:12 +0900

    제이쿼리 적용

--- nemo/src/main/resources/static/login/js/loginmain.js
+++ nemo/src/main/resources/static/login/js/loginmain.js
@@ -1,26 +1,25 @@
 function login() {
 	const requestData = { "userid": $("#userid").val(),"password": $("#password").val() };
 	
-
-    fetch("/login",{
-		method: "POST",
+	$.ajax({
+		type: 'post',
+		url: '/login',
 		headers: {
-			"Content-Type": "application/json",
+			"Content-Type": "application/json"
 		},
-		body: JSON.stringify(requestData),
-	})
-    .then(response => response.text())
-    .then(data => {
-		var loginyn = data;
-
-		if(loginyn == 'Y'){
-			var link = 'http://localhost:8090/';
-			location.replace(link);
-		}
-		else{
-			alert("로그인정보가 존재하지 않습니다.");
-			$("#password").val('');
-			return;
+		data: JSON.stringify(requestData),
+		success: function(data) {
+			var loginyn = data;
+			
+			if(loginyn == 'Y'){
+				var link = 'http://localhost:8090/';
+				location.replace(link);
+			}
+			else{
+				alert("로그인정보가 존재하지 않습니다.");
+				$("#password").val('');
+				return;
+			}
 		}
 	});
 }
